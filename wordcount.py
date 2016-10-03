abfdea4630f67b4e94ff2e790724f200377a0d84
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 12:34:05 -0700

    Created function word_count to scan through a text file and create a dictionary of word and count pairs.

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,24 @@
+# This function is simple and does not account for the punctuation issues
+# def word_count(file_name):
+# """Function takes in a text file reads though each line and creates a dicitonary word and word count pairs.
+#     Returns None but prints the key-value pairs """
+#     file_content = open(file_name)
+#     word_count_dict = {}
+#     for file_line in file_content:
+#         #clean_line is a list of words
+#         clean_line = file_line.rstrip().split(" ")
+
+#         for word in clean_line:
+#             word_count_dict[word] = word_count_dict.get(word, 0) + 1
+   
+#         for word_key in sorted(word_count_dict):
+#             print word_key, word_count_dict[word_key]
+
+
+# word_count("test.txt")
+
+
+
 def word_count(file_name):
     file_content = open(file_name)
     word_count_dict = {}
@@ -6,13 +27,15 @@ def word_count(file_name):
         clean_line = file_line.rstrip().split(" ")
 
         for word in clean_line:
-            word_count_dict[word] = word_count_dict.get(word, 0) + 1
+            word_count_dict[lower(word)] = word_count_dict.get(word, 0) + 1
    
         for word_key in sorted(word_count_dict):
             print word_key, word_count_dict[word_key]
 
 
 word_count("test.txt")
+
+
 
 
 
